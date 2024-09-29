1d60f6cd3bc4d2db1ccde3f1e5440f8b8ac67211
Rennan Felipe <rennanfbcsp@gmail.com>
2024-09-29 12:14:47 -0300

    Implementação da validação de caracteres Brasileiros

--- backend/src/controllers/traininggeneration.js
+++ backend/src/controllers/traininggeneration.js
@@ -9,6 +9,7 @@ const jsonTrainingGenerationIsEmpty = require('../utils/jsonTrainingGenerationIs
 const jsonHasCorrectEntries = require('../utils/jsonHasCorrectEntries');
 const jsonExercisesIsEmpty = require('../utils/jsonExercisesIsEmpty');
 const jsonFilledIsValid = require('../utils/jsonFilledIsValid');
+const functionIsValidBrazilianJSON = require('../utils/isValidBrazilianJSON');
 
 module.exports = class traininggeneration{
     static traininggeneration(req, res){
@@ -21,39 +22,44 @@ module.exports = class traininggeneration{
             }else{
                 const validationResult = jsonHasCorrectEntries(requiredKeys, trainingInputs);
                 if(validationResult.success){
-                    let nome = trainingInputs["nome"]
-                    delete trainingInputs["nome"]
-                    postPrompt(prompted(trainingInputs)).then((response) => { 
-                        let formatJson;
-                        try {
-                            formatJson = FormatJson(response)
-                        } catch (error) {
-                            console.log('Não foi possível formatar o json devido ao erro: ' + error);
-                            req.session.skipRateLimit = true;
-                            return res.status(502).json({ error: "Não foi possível gerar o treino no momento, tente novamente em " + process.env.RATE_LIMIT_OF_TRAINING + " minutos"});
-                        }
-                        const requiredKeysDaysOfWeek = ["segunda", "terca", "quarta", "quinta", "sexta", "sabado", "domingo"];
-                        if (jsonIsValid(requiredKeysDaysOfWeek, formatJson)) {                
-                            const requiredProperty = ["nome", "repeticoes", "descanso"];
-                            if(jsonExercisesIsEmpty(formatJson)){
+                    let isValidBrazilianJSON = functionIsValidBrazilianJSON(requiredKeys, trainingInputs)
+                    if(isValidBrazilianJSON.isValid){
+                        let nome = trainingInputs["nome"]
+                        delete trainingInputs["nome"]
+                        postPrompt(prompted(trainingInputs)).then((response) => { 
+                            let formatJson;
+                            try {
+                                formatJson = FormatJson(response)
+                            } catch (error) {
+                                console.log('Não foi possível formatar o json devido ao erro: ' + error);
                                 req.session.skipRateLimit = true;
                                 return res.status(502).json({ error: "Não foi possível gerar o treino no momento, tente novamente em " + process.env.RATE_LIMIT_OF_TRAINING + " minutos"});
                             }
-                            if(jsonFilledIsValid(requiredProperty, formatJson)){ 
-                                req.session.skipRateLimit = false;
-                                formatJson.nome = trainingInputs.nome
-                                formatJson["nome"] = nome
-                                res.setHeader('Authorization', `token ${req.session.token}`);
-                                res.status(200).header('Content-Type', 'application/json').send(formatJson);
+                            const requiredKeysDaysOfWeek = ["segunda", "terca", "quarta", "quinta", "sexta", "sabado", "domingo"];
+                            if (jsonIsValid(requiredKeysDaysOfWeek, formatJson)) {                
+                                const requiredProperty = ["nome", "repeticoes", "descanso"];
+                                if(jsonExercisesIsEmpty(formatJson)){
+                                    req.session.skipRateLimit = true;
+                                    return res.status(502).json({ error: "Não foi possível gerar o treino no momento, tente novamente em " + process.env.RATE_LIMIT_OF_TRAINING + " minutos"});
+                                }
+                                if(jsonFilledIsValid(requiredProperty, formatJson)){ 
+                                    req.session.skipRateLimit = false;
+                                    formatJson.nome = trainingInputs.nome
+                                    formatJson["nome"] = nome
+                                    res.setHeader('Authorization', `token ${req.session.token}`);
+                                    res.status(200).header('Content-Type', 'application/json').send(formatJson);
+                                }else{
+                                    req.session.skipRateLimit = true;
+                                    return res.status(502).json({ error: "Não foi possível gerar o treino no momento, tente novamente em " + process.env.RATE_LIMIT_OF_TRAINING + " minutos"});
+                                }
                             }else{
-                                req.session.skipRateLimit = true;
+                                req.session.skipRateLimit = true; 
                                 return res.status(502).json({ error: "Não foi possível gerar o treino no momento, tente novamente em " + process.env.RATE_LIMIT_OF_TRAINING + " minutos"});
                             }
-                        }else{
-                            req.session.skipRateLimit = true; 
-                            return res.status(502).json({ error: "Não foi possível gerar o treino no momento, tente novamente em " + process.env.RATE_LIMIT_OF_TRAINING + " minutos"});
-                        }
-                    })
+                        })
+                    }else{
+                        return res.status(400).json({ error: "O caractere é inválido " + isValidBrazilianJSON.invalidChar})
+                    }
                 }else{
                     req.session.skipRateLimit = true;
                     return res.status(422).json({ error: "Foram encontrados os seguintes erros: " +  validationResult.errors});
@@ -79,20 +85,24 @@ module.exports = class traininggeneration{
                 if(jsonFilledIsValid(requiredProperty, trainingInputs)){
                     const validationResult = jsonHasCorrectEntries(requiredKeys, trainingInputs);
                     if(validationResult.success){
-                        let nome = req.body.nome
-                        delete req.body['nome']
-                        let pdf
-
-                        await createPDF(req.body, nome).then((response) => {
-                            pdf = response
-                        }).catch((err) => {
-                            console.log(err)
-                        })
-                        req.session.skipRateLimit = false;
-                        setTimeout(() => {
-                            res.set('Content-Type', 'application/pdf');
-                            res.status(200).send(pdf);
-                        }, 2000);
+                        let isValidBrazilianJSON = functionIsValidBrazilianJSON(requiredKeys, trainingInputs)
+                        if(isValidBrazilianJSON.isValid){
+                            let nome = req.body.nome
+                            delete req.body['nome']
+                            let pdf
+                            await createPDF(req.body, nome).then((response) => {
+                                pdf = response
+                            }).catch((err) => {
+                                console.log(err)
+                            })
+                            req.session.skipRateLimit = false;
+                            setTimeout(() => {
+                                res.set('Content-Type', 'application/pdf');
+                                res.status(200).send(pdf);
+                            }, 2000);
+                        }else{
+                            return res.status(400).json({ error: "O caractere é inválido " + isValidBrazilianJSON.invalidChar})
+                        }
                     }else{
                         req.session.skipRateLimit = true;
                         return res.status(422).json({ error: "Foram encontrados os seguintes erros: " +  validationResult.errors});
