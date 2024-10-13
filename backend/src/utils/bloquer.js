be82ef67f1fb711facb71b5b2e28b184e4a271ec
Rennan Felipe <rennanfbcsp@gmail.com>
2024-10-13 15:01:05 -0300

    Retirada de logs e diminuição do tempo de expiração do token

--- backend/src/utils/bloquer.js
+++ backend/src/utils/bloquer.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 module.exports = function bloquer(req, res, next) {
     const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.connection.remoteAddress;
-    console.log("req.headers['x-forwarded-for'] = " + req.headers['x-forwarded-for'])
-    console.log("req.connection.remoteAddress = " + req.connection.remoteAddress)
     let ipsAndConts;
     let currentIpsAndCounts = [];
     let isBlocked = false;
--- backend/src/utils/validateToken.js
+++ backend/src/utils/validateToken.js
@@ -5,35 +5,26 @@ module.exports = function validateToken(req, res, next) {
 
   let tokenAuthorization = req.headers['authorization'];
   let tokenCookies = req.cookies.token;
-  console.log('tokenAuthorization = ' + tokenAuthorization);
-  console.log('tokenCookies ='+ tokenCookies);
+
   if (tokenAuthorization?.startsWith('Bearer ') && tokenCookies) {
-    console.log('PASSO 1'); 
     tokenAuthorization = tokenAuthorization.slice(7, tokenAuthorization.length);
-  } else {
-    console.log('PASSO 2'); 
+  } else { 
     return res.status(401).json({ error: 'Token inválido' });
   }
-  console.log('req.session.token = ' + req.session.token);
-  if(tokenAuthorization === req.session.token){
-    console.log('PASSO 3'); 
+
+  if(tokenAuthorization === req.session.token){ 
     jwt.verify(tokenAuthorization, process.env.SECRET_KEY_JWT, (err) => {
-    console.log('PASSO 4'); 
-      if (err) {
-        console.log('PASSO 5'); 
+      if (err) { 
         return res.status(401).json({ error: 'Token inválido' });
       }
       jwt.verify(tokenCookies, process.env.SECRET_KEY_JWT, (err) => {
-        console.log('PASSO 6'); 
-        if (err) {
-          console.log('PASSO 7'); 
+        if (err) { 
           return res.status(401).json({ error: 'Token inválido' });
         }
         next();
       });
     });
-  }else{
-    console.log('PASSO 8'); 
+  }else{ 
     return res.status(401).json({ error: 'Token inválido' });
   }
   
