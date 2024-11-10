5718042a4a2a29f558ebe2c6661c045bb053aad9
Rennan Felipe <rennanfbcsp@gmail.com>
2024-11-10 13:29:05 -0300

    Alterando o KeyGenerator do rate limit

--- backend/src/utils/limiter.js
+++ backend/src/utils/limiter.js
@@ -7,7 +7,9 @@ module.exports = function limiter (time, messageError, maxRequest){
         handler: (req, res, next) => {
             return res.status(429).header('Content-Type', 'application/json').send({error: messageError});
         },
-        
+        keyGenerator: (req, res) => {
+            return req.headers['x-forwarded-for']?.split(',')[0] || req.connection.remoteAddress
+        },
         skipSuccessfulRequests: false,
         skipFailedRequests: true,
     });
